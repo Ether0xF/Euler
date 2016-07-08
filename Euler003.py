0b61406b93e53eb22a1fff8a1070e6200ec79b55
Ether0xF <etherzhang16@gmail.com>
2016-07-09 02:28:59 +0800

    tiny modifications

--- Euler003.py
+++ Euler003.py
@@ -1,10 +1,11 @@
 """
-The prime factors of 13195 are 5, 7, 13 and 29.
+    The prime factors of 13195 are 5, 7, 13 and 29.
 
-What is the largest prime factor of the number 600851475143?
+    What is the largest prime factor of the number 600851475143?
 """
 
 def largest_prime_factor(num, prime_factor = 2):
+    # refering prime factorization and factor tree.
     while prime_factor ** 2 <= num:
         if prime_factor ** 2 == num:
             return prime_factor
@@ -12,6 +13,7 @@ def largest_prime_factor(num, prime_factor = 2):
             while num % prime_factor == 0:
                 num //= prime_factor
             prime_factor += 1
+    
     return num
 
 print(largest_prime_factor(600851475143))
